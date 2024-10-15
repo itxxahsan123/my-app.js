8fc66df4f29dba9ae0f76cba7f642791a02ab4a3
itxxahsan123 <jacklewis0084@gmail.com>
2024-10-15 22:09:04 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from ECS Fargate!');
+  res.send('Hellooooo from ECS Fargate!');
 });
 
 const port = process.env.PORT || 3000;
